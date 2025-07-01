d5cb9f787a2ac946fb0459f06dd51908c5923c52
KevinPieplu <pieplu.kevin@gmail.com>
2025-07-01 11:10:05 +0200

    feat: ajout du scss et des styles globaux

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
+import "../styles/global.scss";
 
 export function Layout(){
     return(
         <>
-        <Header />
-        <main>
-            <Outlet />
-        </main>
+        <div className='main-container'>
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+        </div>
         <Footer />
     </>
     )
