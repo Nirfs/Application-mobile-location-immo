28c4f729120dab1ea8620977845f752b3092e1c6
KevinPieplu <pieplu.kevin@gmail.com>
2025-07-01 17:52:13 +0200

    feat: page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom"
+import '../styles/notFound.scss'
 export function NotFound(){
-    return <h1> Not found</h1>
+    return(
+        <div className="error-container">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to='/'>Retourner sur la page d’accueil</Link>
+        </div>
+
+)
 }
