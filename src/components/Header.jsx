2abb98262d12d1a7c182f627785ca375f1d19582
KevinPieplu <pieplu.kevin@gmail.com>
2025-07-01 12:04:08 +0200

    feat: composant header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import { NavLink } from 'react-router-dom';
+//image
+import logo from "../assets/logo_kasa.svg"
+
+//style
+import "../styles/header.scss"
 
 export function Header(){
     return(
         <header>
+            <img className="desktop" src={logo} alt='logo de kasa'/>
             <nav>
-                <NavLink to='/'>Acceuil</NavLink>
-                <NavLink to='/about'>A Propos</NavLink>
+                <NavLink className="link" to='/'>Acceuil</NavLink>
+                <NavLink className="link" to='/about'>A Propos</NavLink>
             </nav>
         </header>
     )
